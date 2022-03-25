6b2075afd7df8583810d99f9837b027e86ab417a
Rodrigo P <rportugal@cualit.com>
2022-03-24 23:21:43 -0300

    WIP added basic elements on the UI

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ new Vue({
   el: '#app',
   data () {
     return {
-      
+      courses: [],
+      title: '',
+      time: 0,
+
     }
   },
   methods: {
     
   },
   computed: {
-    
+    totalTime(){
+      return 0;
+    }
   },
   watch: {
     
