8e3ac6ace2c719be1c5a07affddc6cf295418548
Rodrigo P <rportugal@cualit.com>
2022-03-25 23:12:36 -0300

    logically add courses, missing view

--- app.js
+++ app.js
@@ -4,16 +4,48 @@ new Vue({
     return {
       courses: [],
       title: '',
-      time: 0,
-
+      time: '',
+      showSave: false,
+      inputError: false,
     }
   },
   methods: {
-    
+    // OPTIMIZE: is it possible to simplify this method?
+    addNewCourse () {
+      if (!this.inputError){
+        console.log('no error');
+        if (!this.showSave) {
+          this.showSave = true;
+          console.log('save');
+        } else if (this.title && this.time) {
+          this.showSave = false;
+          this.inputError = false;
+          console.log('cancel');
+        } else this.inputError = true;
+      } 
+      if (this.title && this.time) {
+        console.log('add new course');
+        this.inputError = false;
+        this.showSave = false;
+        this.courses.push({
+          title: this.title,
+          time: this.time
+        });
+        this.title = '';
+        this.time = '';
+      }
+    }
   },
   computed: {
+    // FIX: correct this method to work with time
     totalTime(){
-      return 0;
+      let totalTime =  this.courses.reduce((total, course) => {
+        return total + parseInt(course.time);
+      }, 0);
+      return totalTime;
+    },
+    totalCourses(){
+      return this.courses.length;
     }
   },
   watch: {
