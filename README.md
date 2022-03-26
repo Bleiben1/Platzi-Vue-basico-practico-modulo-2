614204a1224cc12d9a7c174e1ce643ee362b74ca
Rodrigo P <rportugal@cualit.com>
2022-03-26 13:07:24 -0300

    required finctionalities covered

--- README.md
+++ README.md
@@ -8,15 +8,16 @@ Ejercicio práctico correspondiente al Modulo 2 del curso 'Curso Básico de Vue.
 
 En este ejercicio se solicita lo siguiente :
 
- - Armar un simple “tracker” de cursos, que permita calcular y registrar la cantidad cursos de Platzi tomados y obtener la suma de las horas invertidas en tu educacion.
+- Armar un simple “tracker” de cursos, que permita calcular y registrar la cantidad cursos de Platzi tomados y obtener la suma de las horas invertidas en tu educacion.
 
 Haz fork de [este](https://platzi.com/clases/1752-vuejs2/24527-ejercicios-de-practica/#:~:text=Haz%20fork%20de-,este,-CodePen%20para%20obtener) CodePen para obtener la estructura basica del proyecto:
 
 Consideraciones:
+
 - La aplicación debe cumplir los siguientes lineamientos:
-    - Un propiedad courses que sea un array y permita almacenar la lista de cursos.
-    - Tener una propiedad title y otra time que se usean para agregar un nuevo curso a la lista, estas propiedades deben estar enlazados a los inputs usando v-model.
-    - Un boton con un metodo addCourse (enlazado con v-on) que permita agregar un nuevo courso usando los valores de title y time.
-    - Una propiedad computada totalTime que recorra toda la lista de cursos y retorne la suma del tiempo invertido en educacion.
-    - Mostrar la lista de cursos tomados, con el titulo de los mismos y las horas de cada uno usando v-for.
-    - Mostrar el total de horas con totalTime, en caso que no existan cursos se debe mostrar un mensaje indicandolo.
+  - Un propiedad courses que sea un array y permita almacenar la lista de cursos.
+  - Tener una propiedad title y otra time que se usen para agregar un nuevo curso a la lista, estas propiedades deben estar enlazados a los inputs usando v-model.
+  - Un boton con un metodo addCourse (enlazado con v-on) que permita agregar un nuevo courso usando los valores de title y time.
+  - Una propiedad computada totalTime que recorra toda la lista de cursos y retorne la suma del tiempo invertido en educacion.
+  - Mostrar la lista de cursos tomados, con el titulo de los mismos y las horas de cada uno usando v-for.
+  - Mostrar el total de horas con totalTime, en caso que no existan cursos se debe mostrar un mensaje indicandolo.
--- app.js
+++ app.js
@@ -7,42 +7,41 @@ new Vue({
       time: '',
       showSave: false,
       inputError: false,
+      formatError: false,
     }
   },
   methods: {
     // OPTIMIZE: is it possible to simplify this method?
     addNewCourse () {
-      if (!this.inputError){
-        console.log('no error');
+      if (!this.inputError) {
         if (!this.showSave) {
           this.showSave = true;
-          console.log('save');
-        } else if (this.title && this.time) {
+        } else if (this.title && this.time || !this.title && !this.time) {
           this.showSave = false;
           this.inputError = false;
-          console.log('cancel');
         } else this.inputError = true;
       } 
       if (this.title && this.time) {
-        console.log('add new course');
         this.inputError = false;
-        this.showSave = false;
-        this.courses.push({
-          title: this.title,
-          time: this.time
-        });
-        this.title = '';
-        this.time = '';
+        if (this.time.match(/^[0-9:]+$/)) {
+          this.formatError = false;
+          this.showSave = false;
+          this.courses.push({
+            title: this.title,
+            time: timeAmount.setTime(this.time),
+          });
+          this.title = '';
+          this.time = '';
+        } else this.formatError = true;
       }
     }
   },
   computed: {
-    // FIX: correct this method to work with time
     totalTime(){
       let totalTime =  this.courses.reduce((total, course) => {
-        return total + parseInt(course.time);
+        return timeAmount.sumTime(total, course.time);
       }, 0);
-      return totalTime;
+      return timeAmount.setTime(totalTime);
     },
     totalCourses(){
       return this.courses.length;
@@ -52,3 +51,47 @@ new Vue({
     
   }
 })
+
+/**
+ * allow to work with amounts of time
+ */
+const timeAmount = {
+  hour: "h",
+  minute: "m",
+  second: "s",
+
+  setTime(timeString){
+    let time = timeString.split(':');
+    this.hour = time[0];
+    time[1] ? this.minute = time[1] : this.minute = 0;
+    time[2] ? this.second = time[2] : this.second = 0;
+    return this.getTime();
+  },
+
+  getTime(){
+    return (this.hour + ":" + ("0" + this.minute).slice(-2) + ":" + ("0" + this.second).slice(-2));
+  },
+
+  sumTime(time1, time2){
+    let time1Array = time1.toString().split(':');
+    let time2Array = time2.toString().split(':');
+    let hour = parseInt(time1Array[0]) + parseInt(time2Array[0]);
+    let minute = parseInt(time2Array[1]);
+    if (time1Array[1]) minute += parseInt(time1Array[1]);
+    let second = parseInt(time2Array[2]);
+    if (time1Array[2]) second += parseInt(time1Array[2]);
+
+    if (second > 59) {
+      second -= 60;
+      minute += 1;
+    } !second ? second = 0 : second;
+
+    if (minute > 59) {
+      minute -= 60;
+      hour += 1;
+    } !minute ? minute = 0 : minute;
+
+    return this.setTime(hour + ":" + minute + ":" + second);
+  }
+
+}
